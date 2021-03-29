b954a23e936c7b28c3d03f53072bdcc51b76680e
Bogdan Petridean <petridean_bogdan@yahoo.com>
2021-03-29 14:17:13 +0300

    parent 19

--- child/src/main/java/XXX.java
+++ child/src/main/java/XXX.java
@@ -2,7 +2,7 @@ public class XXX {
 // wwewewew sdasdsa
 
     public static void main (String[] args) {
-        System.out.println("Just a log...sdsdss");
+        System.out.println("Just a log...sdsdssdsds");
     }
 }
 
