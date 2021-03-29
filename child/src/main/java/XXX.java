229a118d295c140d644194b679b3d768ebca4137
Bogdan Petridean <petridean_bogdan@yahoo.com>
2021-03-29 14:29:37 +0300

    xxx1

--- child/src/main/java/XXX.java
+++ child/src/main/java/XXX.java
@@ -2,7 +2,7 @@ public class XXX {
 // wwewewew sdasdsa
 
     public static void main (String[] args) {
-        System.out.println("Just asasa loZsaag...sdsdssdsds");
+        System.out.println("Just asasa loZsaaasasag...sdsdssdsds");
     }
 }
 
